e8f40e7fc048aeaa6188a2a0f7018029e1ba28f8
Angel <angelezequielojeda01@gmail.com>
2026-01-15 14:32:09 -0300

    tirar-skin json.parse deletado

--- api/tirar-skin.js
+++ api/tirar-skin.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
       return res.status(405).json({ error: 'Método no permitido' })
     }
 
-    const { user_id } = JSON.parse(req.body)
+    const { user_id } = req.body
 
     if (!user_id) {
       return res.status(400).json({ error: 'Falta user_id' })
